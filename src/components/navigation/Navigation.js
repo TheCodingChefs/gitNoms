bbb6cedf05624238d51e2e638cbcdddcfa848af1
collenpw <collenpw13@gmail.com>
2021-07-14 15:55:37 -0400

    Add search bar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -10,9 +10,10 @@ export default function Navigation() {
                 </Navbar.Brand>
                 <Nav className="navigation-bar">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#search">Search</Nav.Link>
+                    <Nav.Link href="#search">Advanced Search</Nav.Link>
                     <Nav.Link href="#add">Add</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
+                    <input placeholder='Search via keyword'type="text" />
                 </Nav>
             </Container>
         </Navbar>
