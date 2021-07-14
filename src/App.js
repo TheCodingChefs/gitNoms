54209f3c78098b2417a29c98881b37b06b33519a
collenpw <collenpw13@gmail.com>
2021-07-13 20:18:03 -0400

    Clean up practice

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Collen</h1>
-        <h1>Steven</h1>
-        <h1>Anselem</h1>
-        <h1>Woo-Sik</h1>
-        <h1>Dr. Pepper</h1>
-        <h1>Lemonade</h1>
-        <h1>Stella Artois</h1>
-        <h1>Glass Coke</h1>
+        
     </div>
   );
 }
