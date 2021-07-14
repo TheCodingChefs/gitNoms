3cbe8f127202b414ca669d68ef1d3905d18787d2
yetifunk <charlesdemar87@gmail.com>
2021-07-14 13:55:26 -0600

    working form functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import { Route } from 'react-router-dom'
 import Navigation from './components/navigation/Navigation';
 import Display from './components/display/Display'
+import Form from './components/add/Add'
 
 function App() {
   return (
     <div className="App">
         <Navigation />
         <Display />
+        {/* <Route path='/add' component={Form}/> */}
     </div>
   );
 }
