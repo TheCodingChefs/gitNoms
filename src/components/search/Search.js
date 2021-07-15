30280c2f6a68b1a43baae4cd51fa25f3bf490d4d
collenpw <collenpw13@gmail.com>
2021-07-15 10:16:54 -0400

    Save to pull other changes

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,42 @@
+import { useState, useEffect } from 'react';
+
 const Search = ({search}) => {
+
+    const API_URL = process.env.API_URL || 'http://localhost:4000/recipes';
+
+    const [results, setResults] = useState([]);
+    const [filterResults, setFilterResults] = useState([]);
+
+    const getRecipes = async () => {
+        try {
+            const res = await fetch (API_URL);
+            const data = await res.json();
+            setResults(data);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    
+    useEffect(() => {
+        
+        getRecipes();
+        
+    }, []);
+    
+    useEffect(() => {
+        const filterByName = (arr) => {
+            if (!arr) return;
+            console.log(arr);
+            // setFilterResults([...filterResults, arr.filter(recipe => recipe.name.contains(search))]);
+            console.log(filterResults);
+        }
+
+        filterByName(results);
+
+    }, [results])
+
     return (
         <div>
             {search}
