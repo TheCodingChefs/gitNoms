021edbc61cd4f338b4d5f2ac39c610de7f62b7be
yetifunk <charlesdemar87@gmail.com>
2021-07-15 14:23:37 -0600

    form styling changes

--- src/components/edit/RecipeForm.js
+++ src/components/edit/RecipeForm.js
@@ -9,29 +9,29 @@ const RecipeForm = ({handleSubmit, values, setValues, type }) => {
   }
 
   return (
-    <form onSubmit={(e) => handleSubmit(e, values)}>
+    <form onSubmit={(e) => handleSubmit(e, values)} className="recipe-form">
       <div className='edit-title'>
-        <label htmlFor='title'>Title</label>
+        <label htmlFor='title'>Title</label><br></br>
         <input type='text' id='title' value={values.title} onChange={handleChange} placeholder='Title' required />
       </div>
 
       <div className='edit-ingredients'>
-        <label htmlFor='ingredients'>Ingredients</label>
-        <textarea id='ingredients' value={values.ingredients} onChange={handleChange} placeholder='Ingredients' required />
+        <label htmlFor='ingredients'>Ingredients</label><br></br>
+        <textarea id='ingredients' value={values.ingredients} onChange={handleChange} placeholder='Ingredients' required rows="5" cols="50" />
       </div>
 
       <div className='edit-directions'>
-        <label htmlFor='directions'>Directions</label>
-        <textarea id='directions' value={values.directions} onChange={handleChange} placeholder='Directions' required />
+        <label htmlFor='directions'>Directions</label><br></br>
+        <textarea id='directions' value={values.directions} onChange={handleChange} placeholder='Directions' required rows="5" cols="50" />
       </div>
 
       <div className='edit-cuisineType'>
-        <label htmlFor='cuisineType'>Cuisine</label>
+        <label htmlFor='cuisineType'>Cuisine</label><br></br>
         <input type='text' id='cuisineType' value={values.cuisineType} onChange={handleChange} placeholder='Cuisine Type' required />
       </div>
 
       <div className='edit-author'>
-        <label htmlFor='author'>Author</label>
+        <label htmlFor='author'>Author</label><br></br>
         <input type='text' id='author' value={values.author} onChange={handleChange} placeholder='Author' required />
       </div>
 
