7236132363620767f5e0ba59615996c3a44c7429
collenpw <83591226+collenpw@users.noreply.github.com>
2021-07-15 12:13:08 -0400

    Revert "styled the add form component"

--- src/components/add/Add.js
+++ src/components/add/Add.js
@@ -37,15 +37,14 @@ export default function Add() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className="addContainer">
+            <div>
                 <label htmlFor="title">Title</label>
                 <input type="text" required id="title" value={values.title} onChange={handleChange}/>
             </div>
-            <div className="addContainer">
+            <div>
                 <label htmlFor="cuisineType">Cuisine Type</label>
                 <input type="text" required id="cuisineType" value={values.cuisineType} onChange={handleChange}/>
             </div>
-            <div className="addContainer">
             <div className="formatting-directions">
                 <p>Format Ingredients and Directions with an Asterisk between each item/step.</p>  
             </div>
@@ -54,14 +53,6 @@ export default function Add() {
                 <input type="text" required id="ingredients" value={values.ingredients} onChange={handleChange}/>
                 <p>Example: Ingredient One*Ingredient Two*Ingredient Three*etc...</p>
             </div>
-            <div className="addContainer">
-                <label htmlFor="directions">Directions</label >
-                <input type="text"  required id="directions" value={values.directions} onChange={handleChange} />
-                <p>Format Directions with an Asterisk between each step.</p>
-                <p >Example: Direction number one*Direction number two*Direction number three*etc...</p>
-            </div>
-            <div className="submit">
-
             <div>
                 <label htmlFor="directions">Directions</label>
                 <input type="text" required id="directions" value={values.directions} onChange={handleChange}/>
@@ -71,9 +62,7 @@ export default function Add() {
                 <label htmlFor="author">Author</label>
                 <input type="text" id="author" value={values.author} onChange={handleChange}/>
             </div>
-
             <input type='submit' value='Add Recipe' />
-            </div>
         </form>
     )
 }
