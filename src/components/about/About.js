f7728cf1a8e1ee8659732c9e9471a201f5ba3b0c
yetifunk <charlesdemar87@gmail.com>
2021-07-15 13:06:02 -0600

    additional about page styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,7 +14,7 @@ function About() {
             <div className='gitNoms'>
                 <h2>gitNoms</h2>
 
-                <p>Living in this pandemic world, more and more people are becoming the home cook. The gitNoms is an online recipe application. Users can easily upload, edit, or delete their own receipes. The gitNoms is built by the Coding Chefs from 4 different time zones!
+                <p className="about-text">Living in this pandemic world, more and more people are becoming the home cook. The gitNoms is an online recipe application. Users can easily upload, edit, or delete their own receipes. The gitNoms is built by the Coding Chefs from 4 different time zones!
 
                 </p>
             </div>
@@ -23,25 +23,25 @@ function About() {
                 <h2>Introducing the Coding Chefs Team!</h2>
                 <div className='teamMembers'>
                     <div className='steven'>
-                        <img src={stevenProfile} alt='steven profile picture' width='150px' height='150px' />
+                        <img src={stevenProfile} alt='steven profile picture' width='150px' height='150px' className="profile-img"/>
                         <h3>Steven Semco</h3>
                         <h5>Colorado Springs, CO</h5>
                         <h5>{utcHr-6+':'+utcMin+' '}MT</h5>
                     </div>
                     <div className='woosik'>
-                        <img src={woosikProfile} alt='woo-sik profile picture' width='150px' height='150px' />
+                        <img src={woosikProfile} alt='woo-sik profile picture' width='150px' height='150px' className="profile-img" />
                         <h3>Woo-Sik Choi</h3>
                         <h5>Austin, TX</h5>
                         <h5>{utcHr-5+':'+utcMin+' '}CT</h5>
                     </div>
                     <div className='collen'>
-                        <img src={collenProfile} alt='collen profile picture' width='150px' height='150px' />
+                        <img src={collenProfile} alt='collen profile picture' width='150px' height='150px' className="profile-img" />
                         <h3>Collen Whitney</h3>
                         <h5>Harrisonburg, VA</h5>
                         <h5>{utcHr-4+':'+utcMin+' '}ET</h5>
                     </div>
                     <div className='anselem'>
-                        <img src={anselemProfile} alt='anselem profile picture' width='150px' height='150px' />
+                        <img src={anselemProfile} alt='anselem profile picture' width='150px' height='150px' className="profile-img" />
                         <h3>Anselem Ogbennia</h3>
                         <h5>Los Angeles, CA</h5>
                         <h5>{utcHr-7+':'+utcMin+' '}PT</h5>
